96b0e050fc6d9547deecb7a47e574d197b4bb601
zhudongdong <zhudongdong@ysten.com>
2020-05-08 18:53:04 +0800

    feat:目录层级新建;mongoose安装配置

--- config/config.default.js
+++ config/config.default.js
@@ -2,14 +2,9 @@
 
 'use strict';
 
-/**
- * @param {Egg.EggAppInfo} appInfo app info
- */
+
 module.exports = appInfo => {
-  /**
-   * built-in config
-   * @type {Egg.EggAppConfig}
-   **/
+
   const config = exports = {};
 
   // use for cookie sign key, should change to your own and keep security
@@ -17,7 +12,17 @@ module.exports = appInfo => {
 
   // add your middleware config here
   config.middleware = [];
-
+  // 连接mongodb的配置
+  config.mongoose = {
+    client: {
+      url: 'mongodb://127.0.0.1:27017/chart_design_api',
+      options: {
+        useCreateIndex: true,
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+      },
+    },
+  };
   // add your user config here
   const userConfig = {
     // myAppName: 'egg',
